ce383c8deb590e86f3d05627fd9c7f67d6663e7e
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-22 03:14:43 -0700

    delete api create

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipsrkdy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -50,6 +50,13 @@ async function run() {
       const email = req.query.email;
       const query = {email:email};
       const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.delete("/carts/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
     // Send a ping to confirm a successful connection
