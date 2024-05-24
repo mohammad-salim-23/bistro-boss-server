a0e0c64a6061aeabec63e1e1ff6db922e4f85fb4
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-24 07:43:57 -0700

    JWT install and Token create

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const jwt = require('jsonwebtoken');
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
