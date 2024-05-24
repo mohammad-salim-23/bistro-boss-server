9f1f90fced0ae3c21b9d1d99f38aec36b2cc33ea
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-24 03:08:00 -0700

    users get api create

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ async function run() {
     const cartCollection = client.db("bistroDB").collection("carts");
 
     // user related api
+    app.get('/users',async(req,res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
     app.post('/users',async(req,res)=>{
       const user = req.body;
       // insert email if user doesn't exist
