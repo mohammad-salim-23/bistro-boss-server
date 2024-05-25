f70a3e09c57a2bbeae9dc51c1c103744a9825dbc
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-24 21:11:17 -0700

    verify token create

--- index.js
+++ index.js
@@ -31,8 +31,34 @@ async function run() {
     const reviewCollection = client.db("bistroDB").collection("reviews");
     const cartCollection = client.db("bistroDB").collection("carts");
 
+    // middleware
+    const verifyToken = (req,res,next)=>{
+       console.log("inside verify token",req.headers);
+       if(!req.headers.authorization){
+        return res.status(401).send({message:'forbidden access'})
+       }
+       const token = req.headers.authorization.split(' ')[1];
+       jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+          return res.status(401).send({message:'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+       })
+      
+    }
+    // jwt related api
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:'1h'
+      });
+      res.send({token});
+    })
+
     // user related api
-    app.get('/users',async(req,res)=>{
+    app.get('/users',verifyToken,async(req,res)=>{
+     
       const result = await userCollection.find().toArray();
       res.send(result);
     })
