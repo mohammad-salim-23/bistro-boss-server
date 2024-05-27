f44a6982f6d579fbbd97705b8a36401815353098
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-27 12:27:16 -0700

    payment intent api create

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -186,6 +187,20 @@ async function run() {
       const query = {_id:new ObjectId(id)}
       const result = await cartCollection.deleteOne(query);
       res.send(result);
+    })
+    // payment intent
+    app.post('/create-payment-intent',async(req,res)=>{
+      const {price} = req.body;
+      const amount = parseInt(price*100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount:amount,
+        currency:'usd',
+        payment_method_types:['card']
+      });
+      res.send({
+        clientSecret:paymentIntent.client_secret
+      })
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
