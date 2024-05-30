be4949c7b06e12fbc058664b18bfa112c1c1f37f
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-30 07:10:19 -0700

    get payment data using by email

--- index.js
+++ index.js
@@ -218,6 +218,15 @@ async function run() {
      res.send({paymentResult,deleteResult});
     })
 
+    app.get('/payment/:email',verifyToken,async(req,res)=>{
+      const query = {email:req.params.email}
+      if(req.params.email!== req.decoded.email){
+        return res.status(403).send({message:'forbidden access'});
+      }
+      const result = await paymentCollection.find().toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
