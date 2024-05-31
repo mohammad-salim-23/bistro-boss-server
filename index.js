b793a1eb9204e9da984b152c34efa42ee3d42ab1
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-31 00:14:19 -0700

    stats or analytics related api create

--- index.js
+++ index.js
@@ -217,8 +217,38 @@ async function run() {
      
      res.send({paymentResult,deleteResult});
     })
+  
+  //  stats or analytics
+   app.get('/admin-stats',verifyToken,verifyAdmin,async(req,res)=>{
+    const users = await userCollection.estimatedDocumentCount();
+    const menuItems = await menuCollection.estimatedDocumentCount();
+    const orders = await paymentCollection.estimatedDocumentCount();
+    
+    // this is not the best way
+    // const payments = await paymentCollection.find().toArray();
+    // const revenue = payments.reduce((total,payment)=>total+payment.price,0)
+    
+    const result = await paymentCollection.aggregate([
+      {
+        $group:{
+          _id:null,
+          totalRevenue:{
+            $sum:'$price'
+          }
+        }
+      }
+    ]).toArray();
+    const revenue = result.length>0 ?result[0].totalRevenue:0;
 
-    app.get('/payment/:email',verifyToken,async(req,res)=>{
+    res.send({
+      users,
+      menuItems,
+      orders,
+      revenue
+    })
+   })
+
+    app.get('/payments/:email',verifyToken,async(req,res)=>{
       const query = {email:req.params.email}
       if(req.params.email!== req.decoded.email){
         return res.status(403).send({message:'forbidden access'});
